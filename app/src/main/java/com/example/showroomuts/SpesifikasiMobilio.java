64113e0a80a721817037e1dafe3e3b0227d407c5
haikalr98 <haikaliqbal98@gmail.com>
2019-10-25 04:08:24 +0700

    progress

--- app/src/main/java/com/example/showroomuts/SpesifikasiMobilio.java
+++ app/src/main/java/com/example/showroomuts/SpesifikasiMobilio.java
@@ -2,7 +2,9 @@ package com.example.showroomuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SpesifikasiMobilio extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SpesifikasiMobilio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spesifikasi_mobilio);
+    }
+
+    public void Button5(View view) {
+        Intent i = new Intent(SpesifikasiMobilio.this, Mobil.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/showroomuts/spesifikasi_jazz.java
+++ app/src/main/java/com/example/showroomuts/spesifikasi_jazz.java
@@ -2,7 +2,9 @@ package com.example.showroomuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class spesifikasi_jazz extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class spesifikasi_jazz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spesifikasi_jazz);
+    }
+
+    public void Button7(View view) {
+        Intent i = new Intent(spesifikasi_jazz.this, Mobil.class);
+        startActivity(i);
     }
 }
