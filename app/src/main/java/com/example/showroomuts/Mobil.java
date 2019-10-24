88a34b7a6d5b7f774e0cf9d06ade3aebdb1c6d4d
haikalr98 <haikaliqbal98@gmail.com>
2019-10-25 03:02:59 +0700

    spesifikasi awal

--- app/src/main/java/com/example/showroomuts/Mobil.java
+++ app/src/main/java/com/example/showroomuts/Mobil.java
@@ -2,7 +2,9 @@ package com.example.showroomuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Mobil extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Mobil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobil);
+    }
+    public void Button3(View view){
+        Intent i = new Intent(Mobil.this, SpesifikasiVios.class);
+        startActivity(i);
     }
 }
