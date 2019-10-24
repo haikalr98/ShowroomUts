cc8952c2d151a0f7fd84ec984e43d2b3701b4d86
haikalr98 <haikaliqbal98@gmail.com>
2019-10-25 01:49:15 +0700

    progress uts

--- app/src/main/java/com/example/showroomuts/MainActivity.java
+++ app/src/main/java/com/example/showroomuts/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.showroomuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button MobilButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobilButton = findViewById(R.id.button);
+
+        MobilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Mobil.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
